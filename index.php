b6c01a79e0da759b2e7d4335d6650834ffe1da9c
Novaedra <novaedra@gmail.com>
2018-11-08 12:27:58 +0100

    index

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ foreach ($movies as $movie) { ?>
 <div class="wrap">
 
   <div class="film">
-
-    <h2><?php echo $movie['title']; ?></h2><br/>
-      <a href="detail.php?id=<?php echo $movie['id']; ?>"><br/>
-        <img src="posters/<?php echo $movie['id'].".jpg" ?>" alt="<?php echo $movie['title'] ?> "><br/>
+    <br/>
+    <h2><?php echo $movie['title']; ?></h2>
+      <a href="detail.php?id=<?php echo $movie['id']; ?>">
+        <img src="posters/<?php echo $movie['id'].".jpg" ?>" alt="<?php echo $movie['title'] ?> ">
       </a>
   </div>
 <?php } ?>
