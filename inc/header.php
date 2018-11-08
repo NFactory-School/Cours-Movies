0ae10ee0f8fba47f5e34c7e4e9109b28e0e19709
unknown <math.delp76@gmail.com>
2018-11-08 15:51:47 +0100

    ttt

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,7 @@
+<?php include 'pdo.php' ?>
+<?php include 'fonction.php' ?>
 <!DOCTYPE HTML>
 <html>
-<?php include 'pdo.php' ?>
 	<head>
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,22 @@
 <nav>
-  <ul>
-    <li> <a href="incription.php">Inscription</a> </li>
-    <li> <a href="connection.php">Connexion</a> </li>
-  </ul>
+  <?php
+                  if (!empty($_SESSION['user']))
+                  {
+                  ?>
+                <ul>
+                  <li><a href="profil.php">Bienvenue <? echo $_SESSION['pseudo'];?></a></li>
+                  <li><a href="deconnection.php">Deconnexion</a></li>
+                </ul>
+                  <?php
+                      }
+                      else
+                      {
+                  ?>
+                <ul>
+                  <li id="services"><a href="incription.php">Inscription</a></li>
+                  <li id="services"><a href="connection.php">Connexion</a></li>
+                </ul>
+                  <?php
+                  }
+                  ?>
 </nav>
